7e65a5fb8a95c66290c415bbc5ec18db5bbec65c
Mariia <mariia.gorobets@takeoff.com>
2022-01-20 18:50:50 +0200

    Add first test

---
+++ tests_run.py
@@ -0,0 +1,3 @@
+import tests_user
+
+tests_user.test_get_users()
--- tests_user.py
+++ tests_user.py
@@ -1,11 +1,14 @@
 import requests
 
+root_url = "http://localhost:5000"
 
 def test_get_users():
-	url = "http://localhost:5000/users"
+	url = f"{root_url}/users"
 	response = requests.get(url)
-	resp_type = type(response.json())
+	body_type = type(response.json())
+	expected_body_type = list
 
-	assert resp_type == list
-
-test_get_users()
+	if body_type == expected_body_type:
+		print(f"Test for get_users request PASSED. Expected data type is {expected_body_type}")
+	else:
+		print(f"Get_users test FAILED. Expected data type: {expected_body_type}. Actual data type: {body_type}")
